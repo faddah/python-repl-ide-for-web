781f140b2c3e31f26f53fd51f7f6da29dead4a69
Faddah Wolf <my_biz@me.com>
2026-01-27 10:54:08 -0800

    Refactored `direction="horizontal"` to `orientation="horizontal"`.

--- client/src/pages/IDE.tsx
+++ client/src/pages/IDE.tsx
@@ -237,7 +237,7 @@ export default function IDE() {
 
         {/* Editor & Preview Area */}
         <div className="flex-1 flex flex-col min-w-0 bg-background">
-          <ResizablePanelGroup direction="horizontal">
+          <ResizablePanelGroup orientation="horizontal">
             
             {/* Editor Panel */}
             <ResizablePanel defaultSize={50} minSize={30}>
