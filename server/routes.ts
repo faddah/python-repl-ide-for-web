bfee5becb201d89ff2b4e5e116fa7761dc8d9dd8
Faddah Wolf <my_biz@me.com>
2026-02-09 15:01:44 -0800

    Corrected import of api to be from `"../shared/shcema"`.

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,7 @@
 import type { Express } from "express";
 import type { Server } from "http";
 import { storage } from "./storage";
-import { api } from "@shared/schema";
+import { api } from "../shared/schema";
 import { z } from "zod";
 
 export async function registerRoutes(
